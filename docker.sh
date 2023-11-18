8039bd21133d2b58f575f330d6ec83ac57f5bb96
Kushan-Nilanga <dknathalage@gmail.com>
2023-11-18 20:08:38 +1100

    add lib fmt

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
-docker build --no-cache -t dknathalage/ueransim-base -t ueransim-base docker/ueransim/ueransim-base
-docker build --no-cache -t dknathalage/ueransim-gnb -t ueransim-gnb docker/ueransim/ueransim-gnb
-docker build --no-cache -t dknathalage/ueransim-ue -t ueransim-ue docker/ueransim/ueransim-ue
+docker build -t dknathalage/ueransim-base -t ueransim-base docker/ueransim/ueransim-base
+docker build -t dknathalage/ueransim-gnb -t ueransim-gnb docker/ueransim/ueransim-gnb
+docker build -t dknathalage/ueransim-ue -t ueransim-ue docker/ueransim/ueransim-ue
 
 docker push dknathalage/ueransim-base
 docker push dknathalage/ueransim-gnb
